b596d6688e7bab61ed483be2352b6b048d5a2948
Terrance Chang <tchang46343@gmail.com>
2020-02-11 22:47:19 -0800

    Added instructions to Landing Page for user database changes.

--- src/Components/HomePage/UserPages/UserHomeScreen/HomeScreen.js
+++ src/Components/HomePage/UserPages/UserHomeScreen/HomeScreen.js
@@ -6,6 +6,20 @@ export default class UserHomeScreen extends Component {
     return (
       <div className="UserHomeScreen">
         <main>
+          <section className="userInstructions">
+            <h1 className="howToUseIntro">Instructions</h1>
+            <p className="howToUse">
+              Welcome, to the database modification page. This page allows users
+              to add, review, and delete items from the database. To start,
+              let's try adding a database item. Click on the add item tile. Once
+              in the Add Item form, you must enter all the required fields and
+              click "POST". Next, let's verify that the item you just added is
+              in the database click on the "Retrieve Items". In the form fill
+              out Vendor and the Part ID. You should be able to view the new
+              item you just added to the database. Finally, you may also choose
+              to delete and update items from the database.{" "}
+            </p>
+          </section>
           <UserOptions />
         </main>
       </div>
