5755064eaa713d66e90bb6b98043541d4dfb0db0
Terrance Chang <tchang46343@gmail.com>
2020-02-20 20:53:14 -0800

    Made correctionsv2

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 3000,
 
   API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/inventory"
-  // "https://coeus-system-inc.herokuapp.com"
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://coeus-system-inc.herokuapp.com"
+  //"http://localhost:8000/inventory"
 };
